1e9c17de62fe040f294d75b520bd2174d9c6babd
pridon <fridontetradze@gmail.com>
2021-10-12 10:57:03 +0400

    useless screenshots and imports

--- src/app/main/countries/countries.service.ts
+++ src/app/main/countries/countries.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
